d444dc7522d952ca64609caca15f3f5219940771
Marko Sudar <mjsudar21@catawba.edu>
2023-05-06 19:30:37 +0200

    Fifth

---
+++ components/utils/Loader.jsx
@@ -0,0 +1,8 @@
+import styles from './Loader.module.css'
+import loader from './media/loading-gif.gif'
+
+export default function Loader(){
+    return <div className={styles.wrapper}>
+        <div className={styles.media}>hahahahahah</div>
+    </div>
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,38 @@
 import '../styles/global.css';
 import PageLayout from "@/components/Layout/PageLayout";
+import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import Loader from "@/components/utils/Loader";
 
 export default function BlogApp({Component, pageProps}) {
+    const [isLoading, setIsLoading] = useState(false);
+    const route = useRouter()
+
+        const router = useRouter();
+
+        const [loading, setLoading] = useState(false);
+
+        useEffect(() => {
+            const handleStart = (url) => (url !== router.asPath) && setLoading(true);
+            const handleComplete = (url) => (url === router.asPath) && setLoading(false);
+
+            router.events.on('routeChangeStart', handleStart)
+            router.events.on('routeChangeComplete', handleComplete)
+            router.events.on('routeChangeError', handleComplete)
+
+            return () => {
+                router.events.off('routeChangeStart', handleStart)
+                router.events.off('routeChangeComplete', handleComplete)
+                router.events.off('routeChangeError', handleComplete)
+            }
+        })
+
     return (
-        <PageLayout>
-            <Component {...pageProps} />
-        </PageLayout>
+        <>
+            {loading && <Loader/>}
+            <PageLayout>
+                <Component {...pageProps} />
+            </PageLayout>
+        </>
     )
 }
