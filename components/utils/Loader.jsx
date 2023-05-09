7525946726365677eaa9d697f2a8302477a3a85a
Marko Sudar <mjsudar21@catawba.edu>
2023-05-09 15:51:32 +0200

    Fifth

--- components/utils/Loader.jsx
+++ components/utils/Loader.jsx
@@ -3,6 +3,8 @@ import loader from './media/loading-gif.gif'
 
 export default function Loader(){
     return <div className={styles.wrapper}>
-        <div className={styles.media}>hahahahahah</div>
+        <div className={styles.media}>
+            <img src='https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca_w200.gif'/>
+        </div>
     </div>
 }
--- pages/_app.js
+++ pages/_app.js
@@ -4,32 +4,35 @@ import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
 import Loader from "@/components/utils/Loader";
 
+function Loading(){
+    const router = useRouter();
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const handleStart = (url) => (url !== router.asPath) && setLoading(true);
+        const handleComplete = (url) => (url === router.asPath) && setLoading(false);
+
+        router.events.on('routeChangeStart', handleStart)
+        router.events.on('routeChangeComplete', handleComplete)
+        router.events.on('routeChangeError', handleComplete)
+
+        return () => {
+            router.events.off('routeChangeStart', handleStart)
+            router.events.off('routeChangeComplete', handleComplete)
+            router.events.off('routeChangeError', handleComplete)
+        }
+    })
+    
+    return <>{loading && <Loader/>}</>
+}
+
 export default function BlogApp({Component, pageProps}) {
-    const [isLoading, setIsLoading] = useState(false);
-    const route = useRouter()
-
-        const router = useRouter();
-
-        const [loading, setLoading] = useState(false);
-
-        useEffect(() => {
-            const handleStart = (url) => (url !== router.asPath) && setLoading(true);
-            const handleComplete = (url) => (url === router.asPath) && setLoading(false);
-
-            router.events.on('routeChangeStart', handleStart)
-            router.events.on('routeChangeComplete', handleComplete)
-            router.events.on('routeChangeError', handleComplete)
-
-            return () => {
-                router.events.off('routeChangeStart', handleStart)
-                router.events.off('routeChangeComplete', handleComplete)
-                router.events.off('routeChangeError', handleComplete)
-            }
-        })
+        
 
     return (
         <>
-            {loading && <Loader/>}
+            <Loading/>
             <PageLayout>
                 <Component {...pageProps} />
             </PageLayout>
